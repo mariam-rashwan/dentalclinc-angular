5d75c2ace3a2fc14c465c2f5657044c6ecc84fa6
mariam-rashwan <mariamrashwan23@gmail.com>
2022-03-14 03:18:52 +0200

    home first-part

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,6 +8,23 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   constructor() { }
+
+
+  isActive: boolean | undefined;
+
+  active() {
+      this.isActive= true;
+    }
+
+  navbarfixed:boolean = false;
+  @HostListener('window:scroll',['$event'])onScroll(){
+    if(window.scrollY >100){
+      this.navbarfixed = true;
+    }
+    else{
+      this.navbarfixed = false;
+    }
+  }
 
   ngOnInit(): void {
   }
